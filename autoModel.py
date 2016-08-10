af6a4530bcebeccaf5cc71eb11be6cff6b3f12f6
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2016-08-10 20:14:49 +0800

    add

--- autoModel.py
+++ autoModel.py
@@ -5,16 +5,24 @@ def judgeNillDict():
 	pass
 
 def parseJSON(dictionary):
+	allKeys = []
 	keys = []
 	for key,value in dictionary.items():
 		
 		if isinstance(value, list):
-			for json in list:
-				# keys.append(parseJSON(json))
-		else:
-			keys.append(key)
-	return keys
+			for json in value:
+				allKeys.append(parseJSON(json))
 
-with open('/Users/xiexiaolong1/pythonCode/json.txt','r') as jsonString:
+		elif isinstance(value, dict):
+			allKeys.append(parseJSON(value))
+
+		keys.append(key)
+	allKeys.append(keys)
+
+	return allKeys
+
+#/Users/xiexiaolong1/Desktop/zy.txt
+#/Users/xiexiaolong1/pythonCode/json.txt
+with open('/Users/xiexiaolong1/Desktop/zy.txt','r') as jsonString:
 	dic = json.load(jsonString)
-	parseJSON(dic);
+	print(parseJSON(dic))
