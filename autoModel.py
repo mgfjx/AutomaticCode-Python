4e33169d9a99293432a1f4b0ca9988a81a16e7d7
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2016-08-10 18:05:34 +0800

    半成品

--- autoModel.py
+++ autoModel.py
@@ -1,1 +1,20 @@
-print("hehe")
+# -*- coding: utf-8 -*-
+import json
+
+def judgeNillDict():
+	pass
+
+def parseJSON(dictionary):
+	keys = []
+	for key,value in dictionary.items():
+		
+		if isinstance(value, list):
+			for json in list:
+				# keys.append(parseJSON(json))
+		else:
+			keys.append(key)
+	return keys
+
+with open('/Users/xiexiaolong1/pythonCode/json.txt','r') as jsonString:
+	dic = json.load(jsonString)
+	parseJSON(dic);
