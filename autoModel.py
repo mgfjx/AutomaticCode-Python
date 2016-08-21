c1b867124c6da448b8e024bcb48aaba2f1f30edf
xiexiaolong <601318482@qq.com>
2016-08-21 15:33:53 +0800

    简化

--- autoModel.py
+++ autoModel.py
@@ -9,12 +9,14 @@ fileName = 'Model' #新建.h .m 文件名
 mark = 'm_' #给字段加标识
 className = 'Model' #模型类名(需要手动修改)
 
+#configuration end
+
 explanation = '''
 /*
  *  autoModel.py
  *  用python写的根据json数据自动创建.h和.m文件脚本，使用前请配置#configuration 中间的全局变量:
  *  eg:
- *  outPutPath = '/Users/xiexiaolong1/Desktop/python/' (#h .m输出路径,不填默认输出到当前用户桌面)
+ *  outPutPath = '/Users/xiexiaolong1/Desktop/python/' (#.h和.m输出路径,不填默认输出到当前用户桌面)
  *  jsonPath = '/Users/xiexiaolong1/Desktop/python/json.txt' (#json数据文件路径)
  *  fileName = 'ContactModel' (#新建.h .m 文件名)
  *  mark = 'm_' (#给字段加标识)
@@ -22,8 +24,6 @@ explanation = '''
  *  github地址:https://github.com/mgfjxxiexiaolong/AutomaticCode-Python
  */\n\n
 '''
-
-#configuration end
 
 import json
 import os
