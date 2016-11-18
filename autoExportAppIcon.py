04b4c1bf3bf882662b185759c68e00f21633b517
xiexiaolong <601318482@qq.com>
2016-11-18 23:56:23 +0800

    自动导出AppIcon的python脚本

--- autoExportAppIcon.py
+++ autoExportAppIcon.py
@@ -19,8 +19,10 @@ ImageName = sys.argv[1]
 originImg = Image.open(ImageName)
 
 # 20x20
+img0 = originImg.resize((20,20), Image.ANTIALIAS)
 img1 = originImg.resize((40,40), Image.ANTIALIAS)
 img2 = originImg.resize((60,60), Image.ANTIALIAS)
+img0.save(outPutPath + 'appIcon20x20.png',"png")
 img1.save(outPutPath + 'appIcon20x20@2x.png',"png")
 img2.save(outPutPath + 'appIcon20x20@3x.png',"png")
 
@@ -33,24 +35,26 @@ img4.save(outPutPath + 'appIcon29x29@2x.png',"png")
 img5.save(outPutPath + 'appIcon29x29@3x.png',"png")
 
 # 40x40
-img6 = originImg.resize((80,80), Image.ANTIALIAS)
-img7 = originImg.resize((120,120), Image.ANTIALIAS)
-img6.save(outPutPath + 'appIcon40x40@2x.png',"png")
-img7.save(outPutPath + 'appIcon40x40@3x.png',"png")
+img6 = originImg.resize((40,40), Image.ANTIALIAS)
+img7 = originImg.resize((80,80), Image.ANTIALIAS)
+img8 = originImg.resize((120,120), Image.ANTIALIAS)
+img6.save(outPutPath + 'appIcon40x40.png',"png")
+img7.save(outPutPath + 'appIcon40x40@2x.png',"png")
+img8.save(outPutPath + 'appIcon40x40@3x.png',"png")
 
 # 60x60
-img8 = originImg.resize((120,120), Image.ANTIALIAS)
-img9 = originImg.resize((180,180), Image.ANTIALIAS)
-img8.save(outPutPath + 'appIcon60x60@2x.png',"png")
-img9.save(outPutPath + 'appIcon60x60@3x.png',"png")
+img9 = originImg.resize((120,120), Image.ANTIALIAS)
+img10 = originImg.resize((180,180), Image.ANTIALIAS)
+img9.save(outPutPath + 'appIcon60x60@2x.png',"png")
+img10.save(outPutPath + 'appIcon60x60@3x.png',"png")
 
 # ipad
-img10 = originImg.resize((76,76), Image.ANTIALIAS)
-img11 = originImg.resize((152,152), Image.ANTIALIAS)
-img12 = originImg.resize((167,167), Image.ANTIALIAS)
-img10.save(outPutPath + 'appIcon76x76.png',"png")
-img11.save(outPutPath + 'appIcon76x76@2x.png',"png")
-img12.save(outPutPath + 'appIcon83.5x83.5@2x.png',"png")
+img11 = originImg.resize((76,76), Image.ANTIALIAS)
+img12 = originImg.resize((152,152), Image.ANTIALIAS)
+img13 = originImg.resize((167,167), Image.ANTIALIAS)
+img11.save(outPutPath + 'appIcon76x76.png',"png")
+img12.save(outPutPath + 'appIcon76x76@2x.png',"png")
+img13.save(outPutPath + 'appIcon83.5x83.5@2x.png',"png")
 
 # 创建Contents.json文件
 
@@ -58,46 +62,16 @@ content = '''
 {
   "images" : [
     {
-      "size" : "40x40",
-      "idiom" : "ipad",
-      "filename" : "appIcon40x40.png",
-      "scale" : "1x"
-    },
-    {
-      "size" : "40x40",
-      "idiom" : "ipad",
-      "filename" : "appIcon40x40@2x.png",
+      "idiom" : "iphone",
+      "size" : "20x20",
+      "filename" : "appIcon20x20@2x.png",
       "scale" : "2x"
     },
     {
-      "size" : "76x76",
-      "idiom" : "ipad",
-      "filename" : "appIcon76x76.png",
-      "scale" : "1x"
-    },
-    {
-      "size" : "76x76",
-      "idiom" : "ipad",
-      "filename" : "appIcon76x76@2x.png",
-      "scale" : "2x"
-    },
-    {
-      "size" : "83.5x83.5",
-      "idiom" : "ipad",
-      "filename" : "appIcon83.5x83.5@2x.png",
-      "scale" : "2x"
-    },
-    {
-      "size" : "29x29",
-      "idiom" : "ipad",
-      "filename" : "appIcon29x29.png",
-      "scale" : "1x"
-    },
-    {
-      "size" : "29x29",
-      "idiom" : "ipad",
-      "filename" : "appIcon29x29@2x.png",
-      "scale" : "2x"
+      "idiom" : "iphone",
+      "size" : "20x20",
+      "filename" : "appIcon20x20@3x.png",
+      "scale" : "3x"
     },
     {
       "size" : "29x29",
@@ -140,11 +114,65 @@ content = '''
       "idiom" : "iphone",
       "filename" : "appIcon60x60@3x.png",
       "scale" : "3x"
+    },
+    {
+      "idiom" : "ipad",
+      "size" : "20x20",
+      "filename" : "appIcon20x20.png",
+      "scale" : "1x"
+    },
+    {
+      "idiom" : "ipad",
+      "size" : "20x20",
+      "filename" : "appIcon20x20@2x.png",
+      "scale" : "2x"
+    },
+    {
+      "size" : "29x29",
+      "idiom" : "ipad",
+      "filename" : "appIcon29x29.png",
+      "scale" : "1x"
+    },
+    {
+      "size" : "29x29",
+      "idiom" : "ipad",
+      "filename" : "appIcon29x29@2x.png",
+      "scale" : "2x"
+    },
+    {
+      "size" : "40x40",
+      "idiom" : "ipad",
+      "filename" : "appIcon40x40.png",
+      "scale" : "1x"
+    },
+    {
+      "size" : "40x40",
+      "idiom" : "ipad",
+      "filename" : "appIcon40x40@2x.png",
+      "scale" : "2x"
+    },
+    {
+      "size" : "76x76",
+      "idiom" : "ipad",
+      "filename" : "appIcon76x76.png",
+      "scale" : "1x"
+    },
+    {
+      "size" : "76x76",
+      "idiom" : "ipad",
+      "filename" : "appIcon76x76@2x.png",
+      "scale" : "2x"
+    },
+    {
+      "size" : "83.5x83.5",
+      "idiom" : "ipad",
+      "filename" : "appIcon83.5x83.5@2x.png",
+      "scale" : "2x"
     }
   ],
   "info" : {
     "version" : 1,
-    "author" : "mgfjxxiexiaolong@gmial.com"
+    "author" : "xcode"
   }
 }
 '''
