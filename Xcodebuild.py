2aafd879c93d172ff1ca510efcc9dca797c3e367
xiexiaolong <601318482@qq.com>
2016-11-28 21:59:05 +0800

    获取项目名称

--- Xcodebuild.py
+++ Xcodebuild.py
@@ -9,11 +9,29 @@ class Xcodebuild(object):
     def __init__(self, plistPath=None):
         self.plistPath = plistPath
 
-    def existXcworkspaceFile(self):
+    @property
+    def existWorkspace(self):
         workspace = os.popen('find . -maxdepth 1 -name *.xcworkspace -print')
-        path = workspace.read()
-        if not path:
-            return
+        workspaceName = workspace.read()
+        if workspaceName:
+            return True
+        else:
+            return False
+
+    def getProjectName(self):
+        workspace = os.popen('find . -maxdepth 1 -name *.xcworkspace -print')
+        workspaceName = workspace.read()[2:]
+        if not workspaceName:
+            project = os.popen('find . -maxdepth 1 -name *.xcodeproj -print')
+            projectName = project.read()[2:]
+            if not projectName:
+                print ('\033[7;31m' + '脚本所在目录没有xcode项目，请检查脚本所在目录。' + '\033[0m')
+            else:
+                return projectName
+        else:
+            return workspaceName
 
 xcode = Xcodebuild()
-print(xcode.existXcworkspaceFile())
+if xcode.existWorkspace:
+    print (xcode.getProjectName())
+# print(xcode.getProjectName())
